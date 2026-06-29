7322ec791d2a3ef4c775b581ae014b5b20cc2ef3
Muhammad Sohaib Arif <Arif_Sohaib@outlook.com>
2026-06-29 12:05:29 -0400

    removed condition in apply and reduce functions and added doc strings to others to allow tool use

--- module-1/local_model/tool_functions.py
+++ module-1/local_model/tool_functions.py
@@ -1,16 +1,32 @@
 """## Tools used in other files"""
 
 from functools import reduce
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 from typing_extensions import Callable
 
 
 def multiply(a: int | float, b: int | float) -> int | float:
+    """
+    multiply two numbers and return the result
+    Args:
+        a: int|float first number
+        b:int:float second number
+    Returns:
+        int|float mult result
+    """
     return a * b
 
 
 def divide(a: int | float, b: int | float) -> int | float:
+    """
+    divide two numbers and return the result
+    Args:
+        a: int|float first number
+        b:int:float second number
+    Returns:
+        int|float mult result
+    """
     if b != 0:
         return a / b
     else:
@@ -18,38 +34,89 @@ def divide(a: int | float, b: int | float) -> int | float:
 
 
 def add(a: int | float, b: int | float) -> int | float:
+    """
+    add two numbers and return the result
+    Args:
+        a: int|float first number
+        b:int:float second number
+    Returns:
+        int|float mult result
+    """
     return a + b
 
 
 def sub(a: int | float, b: int | float) -> int | float:
+    """
+    subtract two numbers and return the result
+    Args:
+        a: int|float first number
+        b:int:float second number
+    Returns:
+        int|float mult result
+    """
     return a - b
 
 
 def pow(a: int | float, b: int | float) -> int | float:
+    """
+    exponent of a given number to a second number and return the result
+    Args:
+        a: int|float first number
+        b:int:float second number
+    Returns:
+        int|float mult result
+    """
     return a**b
 
 
-def split_string(a: str) -> list:
+def split_string(a: str) -> list[str]:
+    """
+    given a string return the result as list of chars
+    Args:
+        a: str : string to split
+    Returns:
+        list of chars
+    """
     return [char for char in a]
 
 
 def equals(a: str | float | int, b: str | float | int) -> bool:
+    """
+    equality operator
+    Args:
+        a : number or string to compare
+        b : second number or string to compare
+    Returns:
+        boolian indicating if the two values are the same
+    """
     if type(a) is not type(b):
         return False
     return a == b
 
 
 def convert_str_to_int(a: str) -> List[int]:
+    """
+    given a string convert each character into an integer and return the result
+    Args:
+        a: string to split and convert to int
+
+    """
     return [ord(x) for x in a]
 
 
 def greater_than(a: float | int, b: float | int) -> bool:
+    """
+    given a number compare it to second number and return if first number is larger than the second number
+    """
     if type(a) is not type(b):
         return False
     return a > b
 
 
 def less_than(a: float | int, b: float | int) -> bool:
+    """
+    given a number compare it to a second number and return true if first number is lower than second
+    """
     if type(a) is not type(b):
         return False
     return a < b
@@ -68,7 +135,7 @@ _SCALAR_OPS = {
 
 
 def apply_over_list(
-    op_name: str, lst: List[int | float], operand: int | float, cond: Optional[bool]
+    op_name: str, lst: List[int | float], operand: int | float
 ) -> List[int | float]:
     """
     Apply a two argument scalar scalar operation to every element of a list
@@ -76,32 +143,41 @@ def apply_over_list(
     if op_name not in _SCALAR_OPS:
         raise ValueError(f"unknown operation {op_name}; choose from {_SCALAR_OPS}")
     func = _SCALAR_OPS[op_name]
-    if cond is not None:
-        return [func(x, operand) for x in lst if cond]
     return [func(x, operand) for x in lst]
 
 
-def reduce_over_list(
-    op_name: str, lst: List[int | float], cond: Optional[bool]
-) -> int | float:
+def reduce_over_list(op_name: str, lst: List[int | float]) -> int | float:
     """
     Apply a two argument scalar operation to a list and reduce the result
     """
     if op_name not in _SCALAR_OPS:
         raise ValueError(f"unknown operation {op_name} choose from {_SCALAR_OPS}")
     func = _SCALAR_OPS[op_name]
-    if cond is not None:
-        cond_list = [x for x in lst if cond]
-        return reduce(lambda acc, x: func(acc, x), cond_list)
     return reduce(lambda acc, x: func(acc, x), lst)
 
 
 def remove_from_end_of_list(lst: List) -> Tuple[List, str | int | float]:
+    """
+    remove a value from the end of the list
+    Args:
+        lst:List list to remove item from
+    Returns:
+        lst: List[str|innt|float]  the remaining list
+        item: str|int|float the value of the list item
+    """
     item = lst.pop()
     return lst, item
 
 
 def remove_from_list(lst: List, item: str | int | float):
+    """
+    remove first instance of a provided value from the list
+    Args:
+        lst: List to remove item from
+        item: the item to remove
+    Returns:
+        lst: List[str|innt|float]  the remaining list
+    """
     try:
         idx = lst.index(item)
         del lst[idx]
