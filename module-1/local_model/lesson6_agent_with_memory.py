11cf9e96cd80117ae77d95390946514b075ceb71
Muhammad Sohaib Arif <Arif_Sohaib@outlook.com>
2026-07-01 01:11:20 -0400

    added memory to previous agent system and added new question to test memory use

---
+++ module-1/local_model/lesson6_agent_with_memory.py
@@ -0,0 +1,70 @@
+# %%
+from typing import List
+
+from IPython.display import Image, Markdown, display
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_core.messages.utils import AnyMessage
+from langchain_ollama import ChatOllama
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, START, MessagesState
+from langgraph.graph.state import StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from test_questions import (
+    complex_question,
+    complex_question_followup,
+    multiple_step_question,
+    simple_question,
+    text_simple_question,
+)
+from tool_functions import tool_list
+
+# %%
+memory = MemorySaver()
+# %%
+llm = ChatOllama(model="gemma4:e4b")
+llm_with_tools = llm.bind_tools(tool_list)
+
+# %%
+sys_message = SystemMessage(
+    content="You are a helpful assistant tasked with selecting and optionally combining arithmatic operations on scalars or vectors using the provided tools."
+)
+
+
+def assistant(state: MessagesState):
+    return {"messages": llm_with_tools.invoke([sys_message] + state["messages"])}
+
+
+builder = StateGraph(MessagesState)
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tool_list))
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges("assistant", tools_condition)
+builder.add_edge("tools", "assistant")
+react_graph = builder.compile(checkpointer=memory)
+
+# %%
+
+
+display(Image(react_graph.get_graph(xray=True).draw_mermaid_png()))
+
+# %%
+config = {"configurable": {"thread_id": "1"}}
+complex_question_mesage: List[AnyMessage] = [HumanMessage(content=complex_question)]
+complex_question_response = react_graph.invoke(
+    {"messages": complex_question_mesage}, config
+)
+# %%
+for m in complex_question_response["messages"]:
+    m.pretty_print()
+# %%
+complex_question_followup_message: List[AnyMessage] = [
+    HumanMessage(content=complex_question_followup)
+]
+complex_question_followup_response = react_graph.invoke(
+    {"messages": complex_question_followup_message}, config
+)
+# %%
+
+for m in complex_question_followup_response["messages"]:
+    m.pretty_print()
+# %%
