1bddd3d68215014ef304bcc3305e1121767e6b7f
Muhammad Sohaib Arif <Arif_Sohaib@outlook.com>
2026-07-14 10:58:34 -0400

    added script to do load and resume more easily

---
+++ module-2-local/lesson_6_load_and_resume.py
@@ -0,0 +1,97 @@
+"""
+addition to the course's module 2, part 6 to allow reloading the graph and invoking from previous conversation
+"""
+# %%
+import sqlite3
+from langchain_ollama import ChatOllama
+from langgraph.graph import StateGraph, END, START
+from langchain.messages import RemoveMessage, SystemMessage, HumanMessage
+from typing import  Literal
+from langgraph.graph import add_messages
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import MessagesState
+from langgraph.graph.state import CompiledStateGraph
+from pathlib import Path
+from langchain_core.runnables import RunnableConfig
+from IPython.display import display, Image, Markdown
+
+
+
+
+# %%
+#conn = sqlite3.connect(":memory",) #in memory sqlite
+db_path = Path("state_db","example.db")
+conn = sqlite3.connect(str(db_path), check_same_thread=False)
+
+
+model = ChatOllama(model="gemma4:e4b", temperature=0.1)
+# %%
+class SummaryState(MessagesState):
+    summary:str
+
+def call_model(state:SummaryState):
+    summary = state.get("summary","")
+    if summary:
+        system_message = f"Summary of conversation earlier: {summary}"
+        messages = add_messages([SystemMessage(content=system_message)],state["messages"])
+    else:
+        messages = state['messages']
+    response = model.invoke(messages)
+    messages = add_messages(messages, [response])
+    return {"messages":messages}
+
+def summarize_conversation(state:SummaryState):
+    summary = state.get("summary","")
+    if summary:
+        summary_message = (f"This is summary of the conversation to date: {summary}\n\nExtend the summary by taking into account the new messages above:")
+    else:
+        summary_message = "Create a summary of the conversation above:"
+    messages = add_messages(state["messages"],[HumanMessage(content=summary_message)])
+    response = model.invoke(messages)
+    delete_messages = [RemoveMessage(id=m.id) for m in messages[:-2]]
+    return {"summary":response.content, "messages":delete_messages}
+
+def should_continue(state:SummaryState)->Literal["summarize", END]:
+    messages = state['messages']
+    if len(messages) > 3:
+        return "summarize"
+    else:
+        return END
+# %%
+workflow = StateGraph(SummaryState)
+workflow.add_node("conversation", call_model)
+workflow.add_node("summarize",summarize_conversation)
+workflow.add_edge(START, "conversation")
+workflow.add_conditional_edges("conversation", should_continue)
+workflow.add_edge("conversation",END)
+memory = SqliteSaver(conn)
+graph = workflow.compile(checkpointer=memory)
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+# %%
+# %%
+
+config = RunnableConfig(configurable={"thread_id":"1"})
+# %%
+state_so_far = graph.get_state(config)
+summary_so_far = state_so_far.values['summary']
+# %%
+display(Markdown(summary_so_far))
+# %%
+def invoke_and_append_message(graph:CompiledStateGraph,config:RunnableConfig, message:str):
+    graph_state = graph.get_state(config)
+    messages = graph_state.values['messages']
+    summary = graph_state.values['summary']
+    messages = add_messages(messages, [HumanMessage(content=message)])
+    new_state = SummaryState(messages=messages, summary=summary)
+    new_response = graph.invoke(input=new_state, config=config)
+    return new_response
+
+# %%
+input_msg = input("ask a follow up from the summary")
+# %%
+new_response = invoke_and_append_message(graph, config, input_msg)
+
+# %%
+display(Markdown(new_response['summary']))
+# %%
