ede30562427a3927cb56ea3b71e54604b3bb2ab8
Muhammad Sohaib Arif <Arif_Sohaib@outlook.com>
2026-07-16 13:14:43 -0400

    added human validation with basic breakpoint

--- module-3-local/part02_human_in_the_loop.py
+++ module-3-local/part02_human_in_the_loop.py
@@ -4,88 +4,12 @@ from langchain_core.runnables import RunnableConfig
 from langchain_core.messages import AIMessage, SystemMessage, HumanMessage, AnyMessage
 from typing import Literal
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph_sdk import get_client
 from langgraph.graph import START, END, MessagesState
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.graph.state import StateGraph
 from IPython.display import display, Image, Markdown
-# %%
-#simple tools
-def multiply(a:float|int, b:float|int)->float|int:
-    """
-    Add a and b
-    returns:
-        float or int
-    """
-    return a * b
-
-def divide(a:float|int, b:float|int)->float|int:
-    """
-    divides a by b
-    Args:
-        a: int or float
-        b: int or float
-    Returns:
-        a divided by b
-    """
-    return a / b
-
-
-def add(a:float|int, b:float|int)->float|int:
-    """
-    add a and b
-    Args:
-        a: int or float
-        b: int or float
-    Returns:
-        a + b
-    """
-    return a + b
-
-
-def sub(a:float|int, b:float|int)->float|int:
-    """
-    subtracts a and b
-    Args:
-        a: int or float
-        b: int or float
-    Returns:
-        a - b
-    """
-    return a -  b
-
-def pow(a:float|int, b:float|int)->float|int:
-    """
-    subtracts a and b
-    Args:
-        a: int or float
-        b: int or float
-    Returns:
-        a to the power of  b
-    """
-    return a **  b
-
-def apply_to_list(a:list[float|int], op:str, operand:float|int)->list[float|int]|str:
-    """
-    applies a function to a list
-    Args:
-        a: list of ints or floats
-        b: list of ints or floats
-        op: the function to apply
-        operand": the value to apply
-    Returns:
-        list of floats or ints after applying operation
-    """
-    available_ops:dict[str, func] = {
-        "add":add,
-        "mul":multiply,
-        "divide":divide,
-        "sub":sub,
-        "pow":pow
-    }
-    if op in available_ops:
-        return  [available_ops[op](x, operand) for x in a]
-    else:
-        return "operator not found"
+from simple_tools import add, multiply, divide, sub, pow, apply_to_list
 # %%
 llm = ChatOllama(model="gemma4:e4b")
 tools = [add,multiply, divide, sub, pow, apply_to_list]
@@ -107,7 +31,11 @@ memory = MemorySaver()
 graph = builder.compile(checkpointer=memory,interrupt_before=["tools"])
 display(Image(graph.get_graph().draw_mermaid_png()))
 # %%
-initial_input = [HumanMessage(content="multiply 1123 by 34")]
+
+complex_question = """
+complex_question = "While the total is less than 10,000 multiple the values in the list [102, 100, 200, 150, 101, 90, 101,100,110,110,] by 10 and keep adding. What does the total come out to BEFORE reaching 10,000. Use the provided tools.
+"""
+initial_input = [HumanMessage(content=complex_question)]
 thread = RunnableConfig(configurable={"thread_id":"1"})
 for event in graph.stream({"messages":initial_input},config=thread,stream_mode="values"):
     event['messages'][-1].pretty_print()
@@ -117,3 +45,26 @@ print(state)
 # %%
 print(state.next)
 # %%
+for event in graph.stream(None, thread, stream_mode="values"):
+    event["messages"][-1].pretty_print()
+
+# %%
+state = graph.get_state(thread)
+
+print(state.next)
+# %%
+for event in graph.stream(None, thread, stream_mode="values"):
+    event['messages'][-1].pretty_print()
+# %%
+initial_input2 = {"messages":HumanMessage(content="multiply 76 by 123")}
+thread2 = RunnableConfig(configurable={"thread_id":"10"})
+for event in graph.stream(initial_input2, thread2, stream_mode="values"):
+    event['messages'][-1].pretty_print()
+# %%
+# run until first interruption. In the previous option we used None to skip instead of ask for approval
+user_approval = input("do you want to call the tool? (yes/no)")
+if user_approval.lower() == 'yes':
+    for event in graph.stream(None, thread, stream_mode="values"):
+        event['messages'][-1].pretty_print()
+else:
+    print("operation cancelled by user")
